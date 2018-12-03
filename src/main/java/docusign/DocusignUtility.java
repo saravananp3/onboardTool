3116eaecd44ccbadf96f1c54f54284d38cf33df8
shankarganesh1234 <shankarganesh.n@platform3solutions.com>
2018-12-03 14:57:34 +0530

    Signorder

--- src/main/java/docusign/DocusignUtility.java
+++ src/main/java/docusign/DocusignUtility.java
@@ -16,6 +16,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DocusignUtility {
 
@@ -27,7 +28,7 @@ public class DocusignUtility {
     private static final String publicKeyFilename = "D:\\shankar\\officeproject\\onboarding_gradle_project\\src\\main\\webapp\\docs\\publickey.txt";
     private static final String privateKeyFilename = "D:\\shankar\\officeproject\\onboarding_gradle_project\\src\\main\\webapp\\docs\\privatekey.txt";
 
-    public void requestDocumentSigning(ByteArrayOutputStream fileOutputStream, Map.Entry<String, String> users) {
+    public void requestIndividualDocumentSigning(ByteArrayOutputStream fileOutputStream, Map.Entry<String, String> users) {
         System.out.println("\nRequestASignatureTest:\n" + "===========================================");
         byte[] fileBytes = null;
 
@@ -89,6 +90,225 @@ public class DocusignUtility {
 
         envelopeDefinition.setRecipients(new Recipients());
         envelopeDefinition.getRecipients().setSigners(new ArrayList<Signer>());
+        signerListTabs.forEach(s -> envelopeDefinition.getRecipients().getSigners().add(s));
+
+
+        // send the envelope (otherwise it will be "created" in the Draft folder
+        envelopeDefinition.setStatus("sent");
+
+        ApiClient apiClient = new ApiClient(BaseUrl);
+
+        try {
+            // IMPORTANT NOTE:
+            // the first time you ask for a JWT access token, you should grant access by making the following call
+            // get DocuSign OAuth authorization url:
+            //String oauthLoginUrl = apiClient.getJWTUri(IntegratorKey, RedirectURI, OAuthBaseUrl);
+            // open DocuSign OAuth authorization url in the browser, login and grant access
+            //Desktop.getDesktop().browse(URI.create(oauthLoginUrl));
+            // END OF NOTE
+
+            apiClient.configureJWTAuthorizationFlow(publicKeyFilename, privateKeyFilename, OAuthBaseUrl, IntegratorKey, UserId, 3600);
+
+            // now that the API client has an OAuth token, let's use it in all
+            // DocuSign APIs
+
+            OAuth.UserInfo userInfo = apiClient.getUserInfo(apiClient.getAccessToken());
+            Assert.assertNotSame(null, userInfo);
+            Assert.assertNotNull(userInfo.getAccounts());
+            Assert.assertTrue(userInfo.getAccounts().size() > 0);
+
+            System.out.println("UserInfo: " + userInfo);
+            // parse first account's baseUrl
+            // below code required for production, no effect in demo (same
+            // domain)
+
+            apiClient.setBasePath(userInfo.getAccounts().get(0).getBaseUri() + "/restapi");
+            Configuration.setDefaultApiClient(apiClient);
+            String accountId = userInfo.getAccounts().get(0).getAccountId();
+
+            EnvelopesApi envelopesApi = new EnvelopesApi();
+
+            EnvelopeSummary envelopeSummary = envelopesApi.createEnvelope(accountId, envelopeDefinition);
+
+            Assert.assertNotNull(envelopeSummary);
+            Assert.assertNotNull(envelopeSummary.getEnvelopeId());
+            Assert.assertEquals("sent", envelopeSummary.getStatus());
+
+            System.out.println("EnvelopeSummary: " + envelopeSummary);
+
+        } catch (ApiException ex) {
+            ex.printStackTrace();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+    }
+
+    public void requestRoutingDocumentSigning(ByteArrayOutputStream fileOutputStream, List<Signer> signers) {
+        System.out.println("\nRequestASignatureTest:\n" + "===========================================");
+        byte[] fileBytes = null;
+
+        fileBytes = fileOutputStream.toByteArray();
+
+        // create an envelope to be signed
+
+        EnvelopeDefinition envelopeDefinition = new EnvelopeDefinition();
+        envelopeDefinition.setEmailSubject("Please Sign the Document");
+        envelopeDefinition.setEmailBlurb("Please Sign the document");
+
+        // add a document to the envelope
+
+        Document document = new Document();
+        String base64Doc = Base64.encodeToString(fileBytes, false);
+        document.setDocumentBase64(base64Doc);
+        document.setName("IntakeReview.pdf");
+        document.setDocumentId("1");
+
+
+        List<Document> documentList = new ArrayList<Document>();
+        documentList.add(document);
+
+        envelopeDefinition.setDocuments(documentList);
+
+        // Add a recipient to sign the document
+
+        List<Signer> signerListTabs = new ArrayList();
+
+        // Create a SignHere tab somewhere on the document for the signer to sign
+
+        SignHere signHere = new SignHere();
+        signHere.setDocumentId("1");
+        signHere.setPageNumber("7");
+        signHere.setRecipientId("1");
+        signHere.setXPosition("450");
+        signHere.setYPosition("700");
+        signHere.anchorXOffset("500");
+        signHere.anchorYOffset("1000");
+
+        List<SignHere> signHereTabs = new ArrayList<SignHere>();
+        signHereTabs.add(signHere);
+        Tabs tabs = new Tabs();
+        tabs.setSignHereTabs(signHereTabs);
+        signers.stream().map(signer -> {
+            signer.setTabs(tabs);
+            return signer;
+        }).collect(Collectors.toList()).stream().forEach(signer -> signerListTabs.add(signer));
+
+        // Above causes issue
+
+        envelopeDefinition.setRecipients(new Recipients());
+        signerListTabs.forEach(s -> envelopeDefinition.getRecipients().getSigners().add(s));
+
+
+        // send the envelope (otherwise it will be "created" in the Draft folder
+        envelopeDefinition.setStatus("sent");
+
+        ApiClient apiClient = new ApiClient(BaseUrl);
+
+        try {
+            // IMPORTANT NOTE:
+            // the first time you ask for a JWT access token, you should grant access by making the following call
+            // get DocuSign OAuth authorization url:
+            //String oauthLoginUrl = apiClient.getJWTUri(IntegratorKey, RedirectURI, OAuthBaseUrl);
+            // open DocuSign OAuth authorization url in the browser, login and grant access
+            //Desktop.getDesktop().browse(URI.create(oauthLoginUrl));
+            // END OF NOTE
+
+            apiClient.configureJWTAuthorizationFlow(publicKeyFilename, privateKeyFilename, OAuthBaseUrl, IntegratorKey, UserId, 3600);
+
+            // now that the API client has an OAuth token, let's use it in all
+            // DocuSign APIs
+
+            OAuth.UserInfo userInfo = apiClient.getUserInfo(apiClient.getAccessToken());
+            Assert.assertNotSame(null, userInfo);
+            Assert.assertNotNull(userInfo.getAccounts());
+            Assert.assertTrue(userInfo.getAccounts().size() > 0);
+
+            System.out.println("UserInfo: " + userInfo);
+            // parse first account's baseUrl
+            // below code required for production, no effect in demo (same
+            // domain)
+
+            apiClient.setBasePath(userInfo.getAccounts().get(0).getBaseUri() + "/restapi");
+            Configuration.setDefaultApiClient(apiClient);
+            String accountId = userInfo.getAccounts().get(0).getAccountId();
+
+            EnvelopesApi envelopesApi = new EnvelopesApi();
+
+            EnvelopeSummary envelopeSummary = envelopesApi.createEnvelope(accountId, envelopeDefinition);
+
+            Assert.assertNotNull(envelopeSummary);
+            Assert.assertNotNull(envelopeSummary.getEnvelopeId());
+            Assert.assertEquals("sent", envelopeSummary.getStatus());
+
+            System.out.println("EnvelopeSummary: " + envelopeSummary);
+
+        } catch (ApiException ex) {
+            ex.printStackTrace();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+    }
+
+
+    public void requestRoutingDocumentSigning1(ByteArrayOutputStream fileOutputStream, List<Signer> signers) {
+        System.out.println("\nRequestASignatureTest:\n" + "===========================================");
+        byte[] fileBytes = null;
+
+        fileBytes = fileOutputStream.toByteArray();
+
+        // create an envelope to be signed
+
+        EnvelopeDefinition envelopeDefinition = new EnvelopeDefinition();
+        envelopeDefinition.setEmailSubject("Please Sign the Document");
+        envelopeDefinition.setEmailBlurb("Please Sign the document");
+
+        // add a document to the envelope
+
+        Document document = new Document();
+        String base64Doc = Base64.encodeToString(fileBytes, false);
+        document.setDocumentBase64(base64Doc);
+        document.setName("IntakeReview.pdf");
+        document.setDocumentId("1");
+
+
+        List<Document> documentList = new ArrayList<Document>();
+        documentList.add(document);
+
+        envelopeDefinition.setDocuments(documentList);
+
+        // Add a recipient to sign the document
+
+        List<Signer> signerListTabs = new ArrayList();
+
+        // Create a SignHere tab somewhere on the document for the signer to sign
+
+        SignHere signHere = new SignHere();
+        signHere.setDocumentId("1");
+        signHere.setPageNumber("7");
+        signHere.setRecipientId("1");
+        signHere.setXPosition("450");
+        signHere.setYPosition("700");
+        signHere.anchorXOffset("500");
+        signHere.anchorYOffset("1000");
+
+        List<SignHere> signHereTabs = new ArrayList<SignHere>();
+        //signHereTabs.add(signHere);
+        Tabs tabs = new Tabs();
+        tabs.setSignHereTabs(signHereTabs);
+        signers.stream().map(signer -> {
+            signer.setTabs(tabs);
+            return signer;
+        }).collect(Collectors.toList()).stream().forEach(signer -> signerListTabs.add(signer));
+
+        // Above causes issue
+
+        envelopeDefinition.setRecipients(new Recipients());
         signerListTabs.forEach(s -> envelopeDefinition.getRecipients().getSigners().add(s));
 
 
