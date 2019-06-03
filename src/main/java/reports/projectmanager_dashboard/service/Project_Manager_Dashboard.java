6d321297ad8c3d826d7e8e15ebf263651e4e7596
Shankar <shankarganesh.n@platform3solutions.com>
2019-06-03 12:38:40 +0530

    project manager package creation

---
+++ src/main/java/reports/projectmanager_dashboard/service/Project_Manager_Dashboard.java
@@ -0,0 +1,15 @@
+package reports.projectmanager_dashboard.service;
+
+import com.google.gson.JsonObject;
+import onboard.DBconnection;
+import org.apache.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Project_Manager_Dashboard {
+    final static Logger logger = Logger.getLogger(Project_Manager_Dashboard.class);
+
+
+}
---
+++ src/main/java/reports/projectmanager_dashboard/servlet/Project_Manager_Servlet.java
@@ -0,0 +1,23 @@
+package reports.projectmanager_dashboard.servlet;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import service.IntakeInformationService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/Project_Manager_Servlet")
+public class Project_Manager_Servlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
