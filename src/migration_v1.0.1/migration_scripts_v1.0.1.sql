2f3098fa2925913a6be3a0d7502f84ea2125e517
Nandakumar3011 <nandakumar.v@platform3solutions.com>
2022-06-16 12:32:40 +0530

    updated script commit
    Signed-off-by: Nandakumar3011 <nandakumar.v@platform3solutions.com>

--- src/migration_v1.0.1/migration_scripts_v1.0.1.sql
+++ src/migration_v1.0.1/migration_scripts_v1.0.1.sql
@@ -1,19 +1,18 @@
 SELECT * FROM decom3sixtytool.Opportunity_Info_Template_Details;
-update `decom3sixtytool`.`Opportunity_Info_Template_Details` set label_name='Business Unit' where label_name='Buisness Unit';
-update `decom3sixtytool`.`Opportunity_Info_Template_Details` set label_name='Business Segment' where label_name='Buisness Segment';
-update `decom3sixtytool`.`Opportunity_Info_Template_Details` set label_name='EMR System,ERP Data,Financial Data,Healthcare Data,HR Data,MR/HR Data,Other Data' where label_name='EMR System,ERP Data,Financial Data,HealthCare Data,HR Data,MR/HR Data,Other Data';
-update `decom3sixtytool`.`Opportunity_Info_Template_Details` set label_name='Please describe your needs for archival and decommission service' where label_name='Please describe your needs for Archival and Decommission Service';
+update `decom3sixtytool`.`Opportunity_Info_Template_Details` set label_name='Business Unit' where column_name='buisnessunit';
+update `decom3sixtytool`.`Opportunity_Info_Template_Details` set label_name='Business Segment' where column_name='buisnesssegment';
+update `decom3sixtytool`.`Opportunity_Info_Template_Details` set options='EMR System,ERP Data,Financial Data,Healthcare Data,HR Data,MR/HR Data,Other Data' where column_name='date_type';
+update `decom3sixtytool`.`Opportunity_Info_Template_Details` set label_name='Please describe your needs for archival and decommission service' where column_name='arcdecomm';
 
 select * from decom3sixtytool.Triage_Info_Template_Details;
-update `decom3sixtytool`.`Triage_Info_Template_Details` set label_name='Business Segment' where label_name='Business Segement';
-update `decom3sixtytool`.`Triage_Info_Template_Details` set label_name='Program or Segment Contact' where label_name='Program or Segement Contact';
-update `decom3sixtytool`.`Triage_Info_Template_Details` set label_name='Mainframe,Distributed - Unix,Windows,hybrid,Others' where label_name='Mainframe,Distibuted - Unix,Windows,hybrid,Others';
+update `decom3sixtytool`.`Triage_Info_Template_Details` set label_name='Business Segment' where column_name='business_Segment';
+update `decom3sixtytool`.`Triage_Info_Template_Details` set label_name='Program or Segment Contact' where column_name='segment_contact';
+update `decom3sixtytool`.`Triage_Info_Template_Details` set options='Mainframe,Distributed - Unix,Windows,hybrid,Others' where column_name='appPlatfrm';
 
 select * from decom3sixtytool.Assessment_Data_Char_Info_Template_Details;
-update `decom3sixtytool`.`Triage_Info_Template_Details` set label_name='Mainframe,Distributed - Unix,Windows,hybrid,Others' where label_name='Mainframe,Distibuted - Unix,Windows,hybrid,Others';
-update `decom3sixtytool`.`Triage_Info_Template_Details` set label_name='Data is in Read Only State (no updates can be made)' where label_name='Data is in Read Only state (no updates CAN be made)';
-update `decom3sixtytool`.`Triage_Info_Template_Details` set label_name='Are there any datasets on mainframe  that this application uses?' where label_name='Are there any datasets on Mainframe  that this application uses?';
-update `decom3sixtytool`.`Triage_Info_Template_Details` set label_name='please describe' where label_name='please decsribe';
+update `decom3sixtytool`.`Assessment_Data_Char_Info_Template_Details` set label_name='Data is in Read Only State (no updates can be made)' where column_name='ReadonlyData';
+update `decom3sixtytool`.`Assessment_Data_Char_Info_Template_Details` set label_name='Are there any datasets on mainframe  that this application uses?' where column_name='DataSetMainframe';
+update `decom3sixtytool`.`Assessment_Data_Char_Info_Template_Details` set label_name='If yes, please describe ' where column_name='plsdescribeStreams';
 
 select * from decom3sixtytool.Assessment_Application_Info_Template_Details;
-update `decom3sixtytool`.`Assessment_Application_Info_Template_Details` set options='Mainframe,Distributed - Unix,Windows,hybrid,Others' where options='Mainframe,Distibuted - Unix,Windows,hybrid,Others';
+update `decom3sixtytool`.`Assessment_Application_Info_Template_Details` set options='Mainframe,Distributed - Unix,Windows,hybrid,Others' where column_name='AssessAppPlatform';
