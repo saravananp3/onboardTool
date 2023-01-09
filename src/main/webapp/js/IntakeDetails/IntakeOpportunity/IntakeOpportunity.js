15e991b95c3f7d62fde8510378a0b43879472148
VETRIVEL-P3 <98636637+VETRIVEL-P3@users.noreply.github.com>
2023-01-09 15:17:49 +0530

    Revert "Control-Label_Numerical_Validation"
    This reverts commit 93df71fc05da02a004aff56085e760502ea89f8a.

--- src/main/webapp/js/IntakeDetails/IntakeOpportunity/IntakeOpportunity.js
+++ src/main/webapp/js/IntakeDetails/IntakeOpportunity/IntakeOpportunity.js
@@ -78,18 +78,11 @@ $(document).ready(function(){
                     var option=sub_option.split(",");
                     for(var i=0;i<option.length;i++) {
                         var select = "";
-                        if(Value!==""){
                         if(Value.includes(option[i])){
                          select = "selected";
                         }
-                        }
-                         if(option[i]!==""){
-                            inputdrop += "<option label=" + option[i] + " class='control-label' for= 'opportunity' " + select + ">" + option[i] + "</option>";
-                            }
-                            else{
-                                inputdrop += "<option label=''class='control-label' for= 'opportunity' " + select + ">" + option[i] + "</option>";
-                            }
-                        }                       
+                        inputdrop += "<option label='" + option[i] + "' class='control-label' for= 'opportunity' "+select+">" + option[i] + "</option>";
+                    }
                     inputdrop +="</select></div>";
                     $('#inputFields').append(inputdrop);
                 }
